a62de46dfb70e85777042a3526f41a28bb128269
Mateus Melo <masteusmelobrandao@gmail.com>
2025-09-20 18:41:13 -0300

    att-04

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 const toggle = document.getElementById("toggle-span");
+const img = document.getElementById("ft-perfil");
 
 toggle.addEventListener("click", () => {
-    toggle.classList.toggle("light");          // move a lua
-    document.body.classList.toggle("light");   // troca as cores do tema
+    toggle.classList.toggle("light");          
+    document.body.classList.toggle("light");   
+
+    // troca a imagem dependendo do tema
+    if (document.body.classList.contains('light')) {
+        img.src = 'imgs/imagem-prf.jpg';
+        img.alt = 'foto1'; // imagem para light mode
+    } else {
+        img.src = 'imgs/foto2.jpeg'; // imagem para dark mode
+        img.alt = 'foto2';
+    }
 });
